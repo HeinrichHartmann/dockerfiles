e4050abbdb30c333d3a276f00da02fb67620d1e6
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2020-11-22 16:28:47 +0100

    .

--- registry/run.sh
+++ registry/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+docker stop registry
+docker rm registry
 # https://docs.docker.com/registry/deploying/
 docker run -d -p 5000:5000 \
        --restart=unless-stopped --name registry \
+       -v "$(pwd)"/certs:/certs \
+       -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
+       -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+       -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
        -v /rpool/docker-registry:/var/lib/registry \
        registry:2
